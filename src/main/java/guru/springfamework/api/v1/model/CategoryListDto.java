2e1ac6b2c5d50d4749a6427685d487f7f6500c49
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-07-25 17:55:48 +0300

    Add CategoryListDto

---
+++ src/main/java/guru/springfamework/api/v1/model/CategoryListDto.java
@@ -0,0 +1,14 @@
+package guru.springfamework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CategoryListDto {
+
+    private List<CategoryDTO> categories;
+
+}
