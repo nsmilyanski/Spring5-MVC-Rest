4af7a5c2b1982ab72ccfeb255d16fe9e667ddfef
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-07-30 13:23:00 +0300

    Add tests for getCustomerById(), createNewCustomer().

--- src/test/java/guru/springfamework/services/CustomerServiceTest.java
+++ src/test/java/guru/springfamework/services/CustomerServiceTest.java
@@ -13,8 +13,12 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 public class CustomerServiceTest {
@@ -47,7 +51,68 @@ public class CustomerServiceTest {
 
         List<CustomerDto> allCustomers = customerService.getAllCustomers();
 
-        Assert.assertEquals(2, allCustomers.size());
+        assertEquals(2, allCustomers.size());
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstName("Sam");
+        customer2.setLastName("Axe");
+
+        when(customerRepository.findAll()).thenReturn(Arrays.asList(customer1, customer2));
+
+        //when
+        List<CustomerDto> customerDTOS = customerService.getAllCustomers();
+
+        //then
+        assertEquals(2, customerDTOS.size());
+
+    }
+
+    @Test
+    public void getCustomerById() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+
+        when(customerRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(customer1));
+
+        //when
+        CustomerDto customerDTO = customerService.getCustomerById(1L);
+
+        assertEquals("Michale", customerDTO.getFirstName());
+    }
+
+    @Test
+    public void createNewCustomer() throws Exception {
+
+        //given
+        CustomerDto customerDTO = new CustomerDto();
+        customerDTO.setFirstName("Jim");
+
+        Customer savedCustomer = new Customer();
+        savedCustomer.setFirstName(customerDTO.getFirstName());
+        savedCustomer.setLastName(customerDTO.getLastName());
+        savedCustomer.setId(1l);
+
+        when(customerRepository.save(any(Customer.class))).thenReturn(savedCustomer);
+
+        //when
+        CustomerDto savedDto = customerService.createNewCustomer(customerDTO);
+
+        //then
+        assertEquals(customerDTO.getFirstName(), savedDto.getFirstName());
+        assertEquals("/api/v1/customer/1", savedDto.getCustomerUrl());
     }
 
     private List<Customer> loadCustomers() {
